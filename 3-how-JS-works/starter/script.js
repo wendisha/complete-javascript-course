97b9b4b01675df69bd0f7c33f86cdb31d5a23c13
Wendy Calderon <wendycalderon@gmail.com>
2019-10-28 14:31:52 -0400

    Practice hoisting

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,17 +1,34 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+//Function declaration (we could call it before or after, and it will still work)
+function calculateAge(year){
+    console.log(2019 - year);
+}
+
+calculateAge(1984);
+
+//Function expression (we can only call it after defining it)
+var retirement = function(year){
+    console.log(65 - (2019 - year));
+}
+
+retirement(1990);
 
 
+//Variables
+console.log(age);       //this one throws undefined
+var age = 18;
+console.log(age);       //this one console.logs 18
 
+function foo(){
+    console.log(age);   //undefined
+    var age = 65;   
+    console.log(age);   //65
+}
 
-
-
-
-
-
-
-
+foo();
+console.log(age);       //18
 
 
 
