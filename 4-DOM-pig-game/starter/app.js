9ad6bc2cdd56948cde2a35f3dd21f822fa0c9983
Wendy Calderon <wendycalderon@gmail.com>
2019-10-30 18:33:06 -0400

    Define variables

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -8,3 +8,12 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+//Instead of having two separate variables for each player's score, let's create one holding both scores
+var scores, roundScore, activePlayer, dice;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+dice = Math.floor(Math.random() * 6) + 1; 
+console.log(dice);
