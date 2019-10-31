80b092899d6b10cd1438358bb199599d115b5cfc
Wendy Calderon <wendycalderon@gmail.com>
2019-10-30 22:20:16 -0400

    Set values to zero

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -22,6 +22,11 @@ activePlayer = 0;
 //Do not display the dice at the beginning 
 document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
 //We use an anonymous function to handle the click. We could've used a callback, definying the funcion and having the event listener call if for us
 document.querySelector('.btn-roll').addEventListener('click', function(){
     var dice = Math.floor(Math.random() * 6) + 1; 
