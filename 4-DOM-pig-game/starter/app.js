beaae81d9388defcd9837814709bf79bc43b3802
Wendy Calderon <wendycalderon@gmail.com>
2019-11-04 11:25:45 -0500

    Add next player functionality

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -61,5 +61,16 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
     scores[activePlayer] += roundScore;
     //Update UI
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    // roundScore += dice;
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-1').textContent = 0;
+
+    //Change background
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
     //Check if player won game
 });
